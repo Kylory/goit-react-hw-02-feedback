e736cc606350abcdc46fc376192f7340dc8070b3
Kyrylo <krllmsk@gmail.com>
2021-07-12 09:29:23 +0300

    added Total and Positive feedback

--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -1,37 +1,65 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
-}
-  
   static defaultProps = {};
   static propTypes = {};
 
-  handleButton = (event) => {
-    // console.log('clicked button', event.target.name);
-    this.setState({good: 5,});
-  }
+  handleButtonGood = event => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
 
+  handleButtonNeutral = event => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+
+  handleButtonBad = event => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
 
   render() {
     return (
       <>
         <p>Please leave feedback</p>
         <div>
-          <button name="good" type="button" onClick={this.handleButton}>Good</button>
-          <button name="neutral" type="button" onClick={this.handleButton}>Neutral</button>
-          <button name="bad" type="button" onClick={this.handleButton}>Bad</button>
+          <button name="good" type="button" onClick={this.handleButtonGood}>
+            Good
+          </button>
+          <button
+            name="neutral"
+            type="button"
+            onClick={this.handleButtonNeutral}
+          >
+            Neutral
+          </button>
+          <button name="bad" type="button" onClick={this.handleButtonBad}>
+            Bad
+          </button>
         </div>
         <p>Statistics</p>
         <p>Good: {this.state.good}</p>
         <p>Neutral: {this.state.neutral}</p>
         <p>Bad: {this.state.bad}</p>
+        <p>Total: {this.state.good + this.state.neutral + this.state.bad}</p>
+        <p>
+          Positive feedback:{' '}
+          {(this.state.good + this.state.neutral + this.state.bad) /
+            this.state.good}
+          %
+        </p>
       </>
-    )
+    );
   }
 }
 
